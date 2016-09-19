af875887443e119fa7e897af9568b7d0bedbe3c8
Jeremie Bonal <jeremie.bonal@gmail.com>
2016-09-19 23:54:05 +0200

    Commented controllers.

--- app/controllers/survey_answers_controller.rb
+++ app/controllers/survey_answers_controller.rb
@@ -1,7 +1,12 @@
+# Controller for our SurveyAnswers.
+# Allows a user to create a SurveyAnswer but we don't wish to
+# let them edit their answers or delete them.
 class SurveyAnswersController < ApplicationController
   before_action :find_survey, only: [:new, :create]
-  
-  # GET /survey_answers/new
+
+  # GET /survey/:survey_id/survey_answers/new
+  #
+  # Displays a form with nested Answer forms.
   def new
     @survey_answer = SurveyAnswer.new
     @survey.questions.order(:position).each do |question|
@@ -9,9 +14,9 @@ class SurveyAnswersController < ApplicationController
     end
   end
 
-  
-  # POST /survey_answers
-  # POST /survey_answers.json
+  # POST survey/:survey_id/survey_answers
+  #
+  # Creates a SurveyAnswer for the Survey :survey_id
   def create
     @survey_answer = @survey.answers.new(survey_answer_params)
 
@@ -20,16 +25,31 @@ class SurveyAnswersController < ApplicationController
     else
       render :new
     end
-  
   end
 
   private
-  
-  def find_survey 
+
+  # Since we are in nested routes (SurveyAnswer nested in Survey), this method
+  # sets @survey to the Survey object we're currently working with.
+  def find_survey
     @survey = Survey.find(params[:survey_id])
   end
 
+  # SurveyAnswer params to avoid people sending in unpredictable possibly
+  # malicious data through the forms.
+  #
+  # Includes the Answer attributes since we nested forms.
   def survey_answer_params
-    params.require(:survey_answer).permit(:survey_id, answers_attributes:[:id, :choice_id, :question_id])
+    params
+      .require(:survey_answer)
+      .permit(
+        :survey_id,
+        answers_attributes:
+          [
+            :id,
+            :choice_id,
+            :question_id
+          ]
+      )
   end
 end
--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,26 +1,36 @@
+# Controller for the Survey s.
+# Allows us to list the surveys ( #index ), create new surveys
+# ( #new / #create ), and to update them ( #login / #edit / #update )
+# the non-CRUD action #login is there to put in a login screen when
+# the user tries to edit a Survey to ensure they have the right to do so.
 class SurveysController < ApplicationController
   before_action :set_survey, only: [:show, :edit, :update, :destroy, :login]
 
   # GET /surveys
-  # GET /surveys.json
+  #
+  # Lists all surveys in two lists :
+  #  - ongoing (end_date not yet passed)
+  #  - done    (end_date passed)
   def index
-    @surveys_current = Survey.where("end_date > ?", Time.now)
-    @surveys_finished = Survey.where("end_date < ?", Time.now)
+    @surveys_current = Survey.where('end_date > ?', Time.now)
+    @surveys_finished = Survey.where('end_date < ?', Time.now)
   end
 
-  # GET /surveys/1
-  # GET /surveys/1.json
+  # GET /surveys/:id
+  #
+  # Displays the survey's info : a list of questions + the answer statistics.
   def show
   end
 
   # GET /surveys/new
+  #
+  # Displays a form with the possibility to add Question s
+  # and subsequent Choice s for said Question s. (Nested forms)
   def new
     @survey = Survey.new
   end
 
-  # GET /surveys/1/edit
   # POST /surveys
-  # POST /surveys.json
   def create
     @survey = Survey.new(survey_params)
 
@@ -31,43 +41,85 @@ class SurveysController < ApplicationController
     end
   end
 
-  # PATCH/PUT /surveys/1
-  # PATCH/PUT /surveys/1.json
-  def update
-    if params[:survey] && params[:survey][:login] && params[:survey][:login][:orig_password] == @survey.password
-      if @survey.update(survey_params)
-        redirect_to @survey, notice: 'Survey was successfully updated.'
-      else
-        render :edit
-      end
-    else
-      render :login
-    end
+  # GET /surveys/:id/login
+  #
+  # Allows us to screen the users trying to edit the form.
+  # Displays a form with a single field : password that will
+  # then be sent to the #edit action.
+  def login
   end
 
+  # GET /surveys/:id/edit
+  # POST /surveys/:id/edit
+  #
+  # Displays the edition form if the user went through the #login process first
+  # and filled in the password correctly, else redirects to #login again.
+  #
+  # The POST route is there to work properly when going through #login first.
   def edit
-    if params[:login] && params[:login][:password] == @survey.password
+    if params[:login] && # Ensure params[:login] is present.
+       params[:login][:password] == @survey.password
+      # Setting an in-page proof user logged in.
       @orig_password = @survey.password
       render :edit
     else
-      render :login
+      # Back to login if the password is wrong.
+      redirect_to :login, notice: 'Please login before editing survey.'
     end
   end
 
-  def login
+  # PATCH/PUT /surveys/:id
+  #
+  # Allows update to a Survey if the right password was provided during #login.
+  def update
+    # Boils down to :
+    # "if the user went through login with right password before editing:
+    #   proceed with update
+    #  else:
+    #   let them log in again"
+    if params[:survey] &&         # Ensures params[:survey] is present.
+       params[:survey][:login] && # Ensures params[:survey][:login] is present.
+       params[:survey][:login][:orig_password] == @survey.password
+      actual_update
+    else
+      redirect_to :login, notice: 'Please provide correct password before updating survey.'
+    end
   end
-  # DELETE /surveys/1
-  # DELETE /surveys/1.json
-  
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_survey
-      @survey = Survey.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def survey_params
-      params.require(:survey).permit(:title, :end_date, :password, :description, questions_attributes: [:id, :content, :position, choices_attributes: [:content, :id]])
+  # Before each action that is related to a specific Survey
+  # (every single one except #index ), fetches the Survey
+  # corresponding to the :id found in the URL and puts it in
+  # the @survey variable.
+  def set_survey
+    @survey = Survey.find(params[:id])
+  end
+
+  # Makes sure the parameters we get from forms and others only
+  # contain data we actually want.
+  # Also allows the nested attributes of Question s and of the
+  # Choice s nested inside said Question s.
+  def survey_params
+    params.require(:survey)
+          .permit(
+            :title, :end_date, :password, :description,
+            questions_attributes: [
+              :id, :content, :position,
+              choices_attributes: [
+                :id, :content
+              ]
+            ]
+          )
+  end
+
+  # Code for the actual updating of the Survey. Extracted from #update
+  # action to unclutter it - readability purposes.
+  def actual_update
+    if @survey.update(survey_params)
+      redirect_to @survey, notice: 'Survey was successfully updated.'
+    else
+      render :edit
     end
+  end
 end
